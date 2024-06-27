fdf7a06133b2f166b0694c8263b079f15d7ec48f
Suresh <saantalasuresh@gmail.com>
2024-06-27 12:19:29 +0530

    Your commit message here

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express()
 require("dotenv").config();
 const port = process.env.PORT || 3000;
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("server is runing")
 })
 
